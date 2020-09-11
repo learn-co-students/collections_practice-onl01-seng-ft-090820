1b622015d1b3e35ae032957238f5eda104706c5f
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-11 17:44:14 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(num)
+  num.sort
+end
 
+def sort_array_desc(num)
+  num.sort.reverse
+end
+
+def sort_array_char_count(char)
+  char.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_words = []
+  array.each do |word|
+    word.split(" ")
+    word[2] = "$"
+    kesha_words << word
+  end
+  kesha_words
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with? ("a")
+  end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index != 1
+      word + "s"
+    else
+      word
+    end
+  end
+end
